572224ee15b3050e8035c7c5278f69067416325d
Yaocenji <yaocenji@gmail.com>
2026-01-06 20:50:29 +0800

    【新增】字符集控制、建立曲线类

--- include/MyNurbs.h
+++ include/MyNurbs.h
@@ -5,4 +5,35 @@
 
 namespace MyNurbs {
 
+	class RationalCurve {
+	public:
+		// 曲线阶数
+		int degree;
+		// 控制点
+		std::vector<glm::dvec3> control_points;
+		// 节点
+		std::vector<double> knots;
+		// 权重
+		std::vector<double> weights;
+
+		bool Create(int deg,
+			const std::vector<glm::dvec3>& ctrl_pts,
+			const std::vector<double>& kns,
+			const std::vector<double> wts,
+			RationalCurve* curve);
+
+	private:
+		/// <summary>
+		/// 构造函数
+		/// </summary>
+		/// <param name="deg">曲线阶数</param>
+		/// <param name="ctrl_pts">控制点数组</param>
+		/// <param name="kns">节点数组</param>
+		/// <param name="wts">权重数组</param>
+		RationalCurve(int deg,
+			const std::vector<glm::dvec3>& ctrl_pts,
+			const std::vector<double>& kns,
+			const std::vector<double> wts);
+	};
+
 }
--- src/MyNurbs.cpp
+++ src/MyNurbs.cpp
@@ -1,2 +1,40 @@
 #include "MyNurbs.h"
 
+namespace MyNurbs {
+	
+	bool RationalCurve::Create(int deg,
+		const std::vector<glm::dvec3>& ctrl_pts,
+		const std::vector<double>& kns,
+		const std::vector<double> wts,
+		RationalCurve* curve) {
+
+		// 传入指针必须为空
+		if (curve != nullptr) {
+			std::throw_with_nested(std::runtime_error("Curve pointer must be null!");
+			return false;
+		}
+
+		// 检查数量是否符合BSpline曲线规范
+		if (deg < 1) {
+			std::throw_with_nested(std::runtime_error("Curve degree must greater than 0!");
+			return false;
+		}
+		if (ctrl_pts.size() < deg + 1) {
+			std::throw_with_nested(std::runtime_error("Control points number must greater than degree!");
+			return false;
+		}
+		if (kns.size() != ctrl_pts.size() + deg + 1) {
+			std::throw_with_nested(std::runtime_error("Invalid knot vector size.");
+			return false;
+		}
+
+		// 创建曲线
+		curve = new RationalCurve(deg, ctrl_pts, kns, wts);
+		return true;
+	}
+
+	RationalCurve::RationalCurve(int deg,
+		const std::vector<glm::dvec3>& ctrl_pts,
+		const std::vector<double>& kns,
+		const std::vector<double> wts) : degree(deg), control_points(ctrl_pts), knots(kns), weights(wts);
+}
